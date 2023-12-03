caa8fc68ce5c47643fe3e5d87f5fba27786f18bb
fxckxff <valikpletnev2200@gmail.com>
2023-12-03 22:45:10 +0100

    update class modal

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 
